286756bf44e702713967a7874b7640f7881b4b64
zhuzhenjie <devops@kubectl.net>
2023-12-30 02:49:22 +0800

    Refactor logging function into a common script
    The log function previously duplicated in multiple scripts within the 'ddns/aliyun' directory is now refactored into a single common script called 'logger.sh'. This has resulted in a reduction of redundancy, thus potentially making future updates to the logging function simpler and less error prone. The original logging functions in the scripts have been replaced with a source to the new common 'logger.sh' script.

---
+++ commons/commons/logger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+function log() {
+  local remark=$1
+  local msg=$2
+  if [ -z "$remark" ]; then
+    remark="default_remark"
+  fi
+  if [ -z "$msg" ]; then
+    msg="empty msg by log default"
+  fi
+  /bin/bash ../../log.sh "$remark" "$msg"
+}
--- ddns/aliyun/AddDomainRecord.sh
+++ ddns/aliyun/AddDomainRecord.sh
@@ -6,17 +6,7 @@ SHELL_FOLDER=$(
 )
 cd "$SHELL_FOLDER"
 
-function log() {
-  local remark=$1
-  local msg=$2
-  if [ -z "$remark" ]; then
-    remark="default_remark"
-  fi
-  if [ -z "$msg" ]; then
-    msg="empty msg by log default"
-  fi
-  /bin/bash ../../log.sh "$remark" "$msg"
-}
+source ../../commons/commons/logger.sh
 
 RR=$1
 DOMAIN=$2
