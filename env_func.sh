c6d9d3d87b296814e368d694853ba296937e040e
zhenjie zhu <tech@intellij.io>
2024-08-28 14:52:28 +0800

    Simplify logging functions in validate_ip
    Replaced log_info and log_error with a standard log function to streamline the logging mechanism. This change ensures consistency in how log messages are recorded for both valid and invalid IP checks.

--- env_func.sh
+++ env_func.sh
@@ -30,9 +30,9 @@ function validate_ipv4() {
   local regex='^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
 
   if [[ $ip =~ $regex ]]; then
-    log_info "validate_ip" "Valid IP"
+    log "validate_ip" "Valid IP"
   else
-    log_error "validate_ip" "Invalid IP"
+    log "validate_ip" "Invalid IP"
     return 1
   fi
 }
